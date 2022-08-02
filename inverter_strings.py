880a44db41dcf18d4c658573e3939f5bd6fe67fc
Douglas Teixeira <92272919+DougTeixeira@users.noreply.github.com>
2022-08-01 21:50:13 -0300

    Projetos

--- inverter_strings.py
+++ inverter_strings.py
@@ -3,9 +3,12 @@ def inverterPalavra(palavra='python'):
     for letra in palavra:
         fragmento.append(letra)
 
-    print(fragmento)
+    invertido = ''
 
     for i in range(len(fragmento) -1, -1, -1):
-        print(fragmento[i], end=' ')
+        invertido += fragmento[i]
+    
+    return invertido 
 
-inverterPalavra('Desenvolvedor')
+
+print(inverterPalavra('Desenvolvedor'))
